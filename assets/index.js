826ff05a6c3a6e58f0e4eed2525fdb804cc9bb18
Pedro Morim <1180798@isep.ipp.pt>
2021-08-06 19:02:48 +0100

    Added basic electrical components symbols

---
+++ assets/index.js
@@ -0,0 +1,30 @@
+import Resistor from './resistor.svg'
+import Inductor from './inductor.svg'
+import Capacitor from './capacitor.svg'
+import PolarizedCapacitor from './polarized-capacitor.svg'
+import Reactance from './reactance.svg'
+import EarthGround from './earth-gnd.svg'
+import ChassisGround from './chassis-gnd.svg'
+import ACVoltageSource from './vac.svg'
+import DCVoltageSource1 from './vdc1.svg'
+import DCVoltageSource2 from './vdc2.svg'
+import ACCurrentSource from './iac.svg'
+import DCCurrentSource from './idc.svg'
+import Voltmeter from './voltmeter.svg'
+import Ammeter from './ammeter.svg'
+
+export const svgMap = new Map([
+  ['Resistor', Resistor],
+  ['Inductor', Inductor],
+  ['Capacitor', Capacitor],
+  ['PolarizedCapacitor', PolarizedCapacitor],
+  ['Reactance', Reactance],
+  ['Chassis Ground', ChassisGround],
+  ['Earth Ground', EarthGround],
+  ['AC Voltage Source', ACVoltageSource],
+  ['DC Voltage Source', [DCVoltageSource1, DCVoltageSource2]],
+  ['AC Current Source', ACCurrentSource],
+  ['DC Current Source', DCCurrentSource],
+  ['Voltmeter', Voltmeter],
+  ['Ammeter', Ammeter]
+])
