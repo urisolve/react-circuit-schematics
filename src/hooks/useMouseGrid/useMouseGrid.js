524b5b49573c77193b73b2c22ed37d5088ecb16d
Pedro Morim <1180798@isep.ipp.pt>
2021-08-07 01:52:00 +0100

    Fixed some issues with hook

--- src/hooks/useMouseGrid/useMouseGrid.js
+++ src/hooks/useMouseGrid/useMouseGrid.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react'
-import throttle from 'lodash.throttle'
+import { throttle } from 'lodash'
 
 import { snapToGrid } from '../../util'
 
@@ -19,6 +19,7 @@ export const useMouseGrid = (ref, gridSize) => {
   )
 
   useEffect(() => {
+    if (!ref.current) return
     ref.current.addEventListener('mousemove', updateMousePosition)
 
     return () => {
