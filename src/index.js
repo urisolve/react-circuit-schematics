5ef0766e861a54ba353adfb61ab5143830079e6e
Pedro Morim <1180798@isep.ipp.pt>
2021-08-07 01:46:52 +0100

    Fixed library exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from 'react'
+// Hooks
+export * from './hooks/useSchematic'
 
-export const ExampleComponent = ({ text }) => {
-  return <div>Example Component: {text}</div>
-}
+// Components
+export { Connection } from './components/Connection'
+export { ElectricalCore } from './components/ElectricalCore'
+export { Label } from './components/Label'
+export { Node } from './components/Node'
+export { Port } from './components/Port'
+export { Schematic } from './components/Schematic'
