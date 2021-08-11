6b80ae6b469e2258795fc767c25dfa8358799b58
Pedro Morim <1180798@isep.ipp.pt>
2021-08-11 19:28:03 +0100

    Removed useless story

--- src/stories/Schematic.stories.jsx
+++ src/stories/Schematic.stories.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 import React, { useCallback } from 'react'
 import { v4 as uuidv4 } from 'uuid'
-import lodash from 'lodash'
 
 import { Schematic } from '../components/Schematic'
 import { useSchematic } from '../hooks/useSchematic'
@@ -25,73 +24,9 @@ export default {
 
 export const Empty = () => <Schematic width={800} height={500} />
 
-export const JSON_FormatExample = () => {
-  const circuit = {
-    components: [
-      {
-        id: '8c0168ec-07fe-4b2c-96ee-4292ab34cfb2',
-        type: 'DC Voltage Source',
-        position: { x: 200, y: 150 },
-        label: {
-          name: 'U',
-          value: '5',
-          unit: 'V',
-          position: { x: 270, y: 220 }
-        },
-        ports: [
-          {
-            id: '7ed2507f-14fe-45e2-9246-9ee1d36eb442',
-            type: 'hybrid',
-            position: { x: 0.5, y: 0 }
-          },
-          {
-            id: 'c990e78e-2bc5-4afb-92ee-b8a6d1faab0f',
-            type: 'hybrid',
-            position: { x: 0.5, y: 1 }
-          }
-        ]
-      },
-      {
-        id: 'a2475d77-22a6-47b5-b60b-04aa5af69284',
-        type: 'Resistor',
-        position: { x: 400, y: 200, angle: 90 },
-        label: {
-          name: 'R',
-          value: '10k',
-          unit: 'Ω',
-          position: { x: 480, y: 240 }
-        },
-        ports: [
-          {
-            id: 'ddd6efab-f8f2-4565-8010-1270556f1d00',
-            type: 'hybrid',
-            position: { x: 0, y: 0.5 }
-          },
-          {
-            id: '1f7b8784-184b-4733-ae81-53c74b62462c',
-            type: 'hybrid',
-            position: { x: 1, y: 0.5 }
-          }
-        ]
-      }
-    ],
-    nodes: [],
-    connections: [
-      {
-        id: 'fd313c9d-03f2-437d-8462-331e4efc660a',
-        start: '7ed2507f-14fe-45e2-9246-9ee1d36eb442',
-        end: 'ddd6efab-f8f2-4565-8010-1270556f1d00'
-      },
-      {
-        id: 'da7214da-14c6-49ab-9ca6-5b2ed8a184d3',
-        start: '1f7b8784-184b-4733-ae81-53c74b62462c',
-        end: 'c990e78e-2bc5-4afb-92ee-b8a6d1faab0f'
-      }
-    ]
-  }
-
-  const { schematic } = useSchematic(circuit)
-  return <Schematic schematic={schematic} width={800} height={500} />
+export const ReadOnly = () => {
+  const { schematic } = useSchematic(RLC_Circuit)
+  return <Schematic schematic={schematic} width={800} height={500} readOnly />
 }
 
 export const UndoAndRedo = () => {
@@ -130,9 +65,4 @@ export const AddConnections = () => {
       <Schematic schematic={schematic} width={800} height={500} />
     </>
   )
-}
-
-export const ReadOnly = () => {
-  const { schematic } = useSchematic(RLC_Circuit)
-  return <Schematic schematic={schematic} width={800} height={500} readOnly />
 }
